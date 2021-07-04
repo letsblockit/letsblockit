0f2b62b0d4a11364db8c5135c4a8be34ed1539f2
Xavier Vello <xavier.vello@gmail.com>
2021-07-04 19:30:38 +0200

    update vendorsha

--- default.nix
+++ default.nix
@@ -4,6 +4,6 @@ pkgs.buildGoModule.override{
 } {
   pname = "weblock";
   version = "1.0";
-  vendorSha256 = "1fwcaly2x79zw9wpm5v7xxd6q5k8kgbx6drncn84hqcxmvww7kqv";
+  vendorSha256 = "0dv610gilwwqlchqhhxy74717s16v8dzirp6bisvn5ga1qh96fhw";
   src = ./.;
 }
