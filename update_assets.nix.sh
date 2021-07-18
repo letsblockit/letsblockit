5c3d137eaad40cb6421af74dbbb51054c4e8ad9b
Xavier Vello <xavier.vello@gmail.com>
2021-07-18 22:23:35 +0200

    add update_assets.nix.sh script

---
+++ update_assets.nix.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p nodejs -p nodePackages.npm
+#! nix-shell --quiet
+
+# This script updates the assets by running npm
+
+set -euox pipefail
+
+npm install --prefix ./src/assets/
+npm run assets --prefix ./src/assets/
+git add ./data/assets/
