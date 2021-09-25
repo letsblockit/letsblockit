d5337e738b4227588acfd98b208c6bda122e5211
Xavier Vello <xavier.vello@gmail.com>
2021-09-26 00:00:37 +0200

    users: simplify handlers execution flow

--- src/server/user.go
+++ src/server/user.go
@@ -9,19 +9,22 @@ import (
 func (s *Server) userLogin(c echo.Context) error {
 	if getUser(c) != nil {
 		return c.Redirect(http.StatusFound, "/user/account")
-	} else {
-		hc := s.buildHandlebarsContext(c, "Login")
-		return s.pages.render(c, "user-login", hc)
 	}
+	hc := s.buildHandlebarsContext(c, "Login")
+	return s.pages.render(c, "user-login", hc)
 }
 
 func (s *Server) userAccount(c echo.Context) error {
-	if u := getUser(c); u != nil {
-		hc := s.buildHandlebarsContext(c, "My account")
-		hc["verified"] = u.IsVerified()
-		return s.pages.render(c, "user-account", hc)
-	} else {
+	user := getUser(c)
+	if user == nil {
 		// Cannot find user session, redirect to login
 		return c.Redirect(http.StatusFound, "/user/login")
 	}
+
+	hc := s.buildHandlebarsContext(c, "My account")
+	if user.IsVerified() {
+		hc["verified"] = true
+
+	}
+	return s.pages.render(c, "user-account", hc)
 }
