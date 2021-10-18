3483cda23045e01d5a374815453db9ec002045b6
Xavier Vello <xavier.vello@gmail.com>
2021-10-18 20:54:39 +0200

    add logged:true/false tag to latency metric

--- src/server/server.go
+++ src/server/server.go
@@ -206,9 +206,10 @@ func buildDogstatsMiddleware(dsd statsd.ClientInterface) echo.MiddlewareFunc {
 			if err := next(c); err != nil {
 				c.Error(err)
 			}
+			loggedTag := fmt.Sprintf("logged:%t", c.Get(userContextKey) != nil)
 			duration := time.Since(start)
-			_ = dsd.Distribution("letsblockit.request_duration", float64(duration.Nanoseconds()), nil, 1)
-			_ = dsd.Incr("letsblockit.request_count", []string{fmt.Sprintf("status:%d", c.Response().Status)}, 1)
+			_ = dsd.Distribution("letsblockit.request_duration", float64(duration.Nanoseconds()), []string{loggedTag}, 1)
+			_ = dsd.Incr("letsblockit.request_count", []string{loggedTag, fmt.Sprintf("status:%d", c.Response().Status)}, 1)
 			return nil
 		}
 	}
