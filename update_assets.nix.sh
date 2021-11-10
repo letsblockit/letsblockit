f2b277cbaf08ce0ae9a57157e39cc31b8e9c0b01
Xavier Vello <xavier.vello@gmail.com>
2021-11-10 18:52:40 +0100

    update_assets add --watch flag

--- update_assets.nix.sh
+++ update_assets.nix.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p nodejs -p nodePackages.npm -p git
+#! nix-shell -i bash -p nodejs -p nodePackages.npm -p git -p reflex
 #! nix-shell --pure --quiet
 
 # This script updates the assets by running npm
@@ -7,5 +7,13 @@
 set -euox pipefail
 
 npm install --prefix ./src/assets/
+
+if [ "$*" == "--watch" ]; then
+  reflex -r "src/assets" -s -- npm run assets --prefix ./src/assets/
+else
+  npm run assets --prefix ./src/assets/
+  git add ./data/assets/
+fi
+
 npm run assets --prefix ./src/assets/
 git add ./data/assets/
