9827843e5adc8ed016a1a836679871b9360df05a
Xavier Vello <xavier.vello@gmail.com>
2021-11-14 20:38:09 +0100

    document run_auth.sh

--- run_auth.sh
+++ run_auth.sh
@@ -2,4 +2,5 @@
 #! nix-shell -i bash -p "(callPackage ./ory-cli.nix {})"
 #! nix-shell --pure --quiet
 
+# Runs the Ory proxy on the letsblockit-dev project
 ory proxy --port 4000 http://localhost:8765/ --sdk-url https://inspiring-gates-6gd6xqshnz.projects.oryapis.com --no-jwt
