2f2b06f17beb2481a78644a56d5011b45afc3dae
Xavier Vello <xavier.vello@gmail.com>
2022-01-30 15:15:51 +0100

    refactor list rendering logic to src/filters package

---
+++ src/filters/list_test.go
@@ -0,0 +1,94 @@
+package filters
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/suite"
+	"github.com/xvello/letsblockit/src/filters/mocks"
+)
+
+type ListTestSuite struct {
+	suite.Suite
+	repository *Repository
+	logger     *mocks.Mocklogger
+	expectL    *mocks.MockloggerMockRecorder
+}
+
+func (s *ListTestSuite) SetupTest() {
+	c := gomock.NewController(s.T())
+	s.logger = mocks.NewMocklogger(c)
+	s.expectL = s.logger.EXPECT()
+
+	var err error
+	s.repository, err = load(os.DirFS("testdata"))
+	s.NoError(err)
+}
+
+func (s *ListTestSuite) TestRenderEmpty() {
+	buf := &strings.Builder{}
+	list := &List{}
+	s.NoError(list.Render(buf, s.logger, s.repository))
+	s.Equal(`! Title: letsblock.it - 
+! Expires: 1 hour
+! Homepage: https://letsblock.it
+! License: https://github.com/xvello/letsblockit/blob/main/LICENSE.txt
+`, buf.String())
+}
+
+func (s *ListTestSuite) TestRenderOK() {
+	buf := &strings.Builder{}
+	list := &List{
+		Title: "Test list",
+		Instances: []*Instance{{
+			Filter: "hello",
+		}, {
+			Filter: "hello",
+		}, {
+			Filter: "unknown",
+		}, {
+			Filter: "simple",
+			Params: map[string]interface{}{
+				"string_list": []string{"one", "two"},
+			},
+		}},
+	}
+	s.expectL.Warnf(gomock.Any(), "unknown", gomock.Any())
+	s.NoError(list.Render(buf, s.logger, s.repository))
+	s.Equal(`! Title: letsblock.it - Test list
+! Expires: 1 hour
+! Homepage: https://letsblock.it
+! License: https://github.com/xvello/letsblockit/blob/main/LICENSE.txt
+
+! hello
+Hello
+! hello
+Hello
+! unknown
+
+! simple
+one
+two
+`, buf.String())
+}
+
+func (s *ListTestSuite) TestValidateOK() {
+	list := &List{
+		Title: "Test list",
+		Instances: []*Instance{{
+			Filter: "hello",
+		}, {
+			Filter: "simple",
+			Params: map[string]interface{}{
+				"string_list": []string{"one", "two"},
+			},
+		}},
+	}
+	s.NoError(list.Validate())
+}
+
+func TestListTestSuite(t *testing.T) {
+	suite.Run(t, new(ListTestSuite))
+}
--- src/server/list.go
+++ src/server/list.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
@@ -10,76 +9,56 @@ import (
 	"github.com/xvello/letsblockit/src/filters"
 )
 
-const listHeaderTemplate = `! Title: letsblock.it - My filters
-! Expires: 1 hour
-! Homepage: https://letsblock.it
-! License: https://github.com/xvello/letsblockit/blob/main/LICENSE.txt
-`
-
-const filterHeaderTemplate = `
-! %s
-`
-
 func (s *Server) renderList(c echo.Context) error {
 	token, err := uuid.Parse(c.Param("token"))
 	if err != nil {
 		return err
 	}
 
-	var instances []db.GetInstancesForListRow
+	var storedInstances []db.GetInstancesForListRow
 	if err := s.store.RunTx(c, func(ctx context.Context, q db.Querier) error {
-		list, err := q.GetListForToken(ctx, token)
-		if err == db.NotFound {
+		storedList, e := q.GetListForToken(ctx, token)
+		if e == db.NotFound {
 			return echo.ErrNotFound
-		} else if err != nil {
-			return err
-		} else if s.isUserBanned(list.UserID) {
+		} else if e != nil {
+			return e
+		} else if s.isUserBanned(storedList.UserID) {
 			return echo.ErrForbidden
 		}
 
 		if c.Request().Header.Get("Referer") == "" {
-			err = q.MarkListDownloaded(ctx, list.ID)
-			if err != nil {
-				return err
+			e = q.MarkListDownloaded(ctx, storedList.ID)
+			if e != nil {
+				return e
 			}
 		}
 
-		instances, err = q.GetInstancesForList(ctx, list.ID)
-		return err
+		storedInstances, e = q.GetInstancesForList(ctx, storedList.ID)
+		return e
 	}); err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprint(c.Response(), listHeaderTemplate)
-	if err != nil {
-		return err
+	list := &filters.List{Title: "My filters"}
+	var customFilterInstances []*filters.Instance
+	for _, storedInstance := range storedInstances {
+		instance := &filters.Instance{
+			Filter: storedInstance.FilterName,
+			Params: make(map[string]interface{}),
+		}
+		err = storedInstance.Params.AssignTo(&instance.Params)
+		if err != nil {
+			return err
+		}
+		if instance.Filter == filters.CustomRulesFilterName {
+			customFilterInstances = append(customFilterInstances, instance)
+		} else {
+			list.Instances = append(list.Instances, instance)
+		}
+	}
+	if len(customFilterInstances) > 0 {
+		list.Instances = append(list.Instances, customFilterInstances...)
 	}
 
-	var custom []db.GetInstancesForListRow
-	printFilter := func(f *db.GetInstancesForListRow) error {
-		_, e := fmt.Fprintf(c.Response(), filterHeaderTemplate, f.FilterName)
-		if e != nil {
-			return e
-		}
-		params := make(map[string]interface{})
-		if e = f.Params.AssignTo(&params); e != nil {
-			return e
-		}
-		return s.filters.Render(c.Response(), f.FilterName, params)
-	}
-	for _, f := range instances {
-		if f.FilterName == filters.CustomRulesFilterName {
-			custom = append(custom, f)
-			continue
-		}
-		if err := printFilter(&f); err != nil {
-			c.Logger().Warnf("skipping filter %s in list %s: %s", f.FilterName, token, err.Error())
-		}
-	}
-	for _, f := range custom {
-		if err := printFilter(&f); err != nil {
-			c.Logger().Warnf("skipping filter %s in list %s: %s", f.FilterName, token, err.Error())
-		}
-	}
-	return nil
+	return list.Render(c.Response(), c.Logger(), s.filters)
 }
