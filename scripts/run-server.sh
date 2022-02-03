37a64a02dff43fcd62f5e1b88aa2aa38f26c4457
Xavier Vello <xavier.vello@gmail.com>
2022-02-03 15:01:40 +0100

    run-server.sh allow to override ORY_SDK_URL

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -2,5 +2,6 @@
 # This script runs the auth proxy and the server, recompiling the server on code changes.
 ## Run it with `nix run .#run-server`, or install the dependencies manually.
 
-ory proxy --port 4000 http://localhost:8765/ --sdk-url https://inspiring-gates-6gd6xqshnz.projects.oryapis.com --no-jwt &
+export ORY_SDK_URL=${ORY_SDK_URL:-https://inspiring-gates-6gd6xqshnz.projects.oryapis.com}
+ory proxy --port 4000 http://localhost:8765/ --no-jwt &
 reflex -r "(cmd|src|data)/" -s -- go run -race ./cmd/server --reload
