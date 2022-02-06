484b0541b4a13af83636685004370a663f8458bb
Xavier Vello <xavier.vello@gmail.com>
2022-02-06 10:16:29 +0100

    upgrade sqlc

--- nix/sqlc.nix
+++ nix/sqlc.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub, go_1_17 }:
 buildGoModule.override { go = go_1_17; } rec {
   pname = "sqlc";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchFromGitHub {
     owner = "kyleconroy";
     repo = "sqlc";
     rev = "v${version}";
-    sha256 = "sha256-EzV0h5YPaZdsPFjihXX5gDMiWqlCKFVlN39c9/eerAU=";
+    sha256 = "sha256-YlOkjqkhN+4hL1+KJ0TuqcbQXJad/bHZclgpgFPr4to=";
   };
 
-  vendorSha256 = "sha256-lxOrCRyk3zHRw8WZLs0p7pN6dCyOM2pqy3bax8+PCi0=";
+  vendorSha256 = "sha256-MX140KUb+FGtorzOR46NHlWFhfzvlsSbg41sn3XR8Ys=";
   runVend = true; # pg_query_go ships the C headers in its module
   doCheck = false;
 }
