94db474a2c0c9cf9c139e5c8e2a1614b2735fb6e
Xavier Vello <xavier.vello@gmail.com>
2022-02-18 16:34:56 +0100

    Use the abp:subscribe link for setup instructions

--- src/server/helpers.go
+++ src/server/helpers.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/xvello/letsblockit/src/filters"
@@ -30,7 +31,12 @@ func buildHelpers(e echoInterface, assetHash string) map[string]interface{} {
 			return href(e, route, args)
 		},
 		"list_href": func(token string) string {
-			return "https://get.letsblock.it" + href(e, "render-filterlist", token)
+			return listDownloadRef(e, token)
+		},
+		"abp_subscribe_href": func(token string) string {
+			return fmt.Sprintf("abp:subscribe?location=%s&title=%s",
+				url.QueryEscape(listDownloadRef(e, token)),
+				url.QueryEscape("letsblock.it - My filters"))
 		},
 		"lookup_list": func(obj map[string]interface{}, key string) []string {
 			switch values := obj[key].(type) {
@@ -50,6 +56,10 @@ func buildHelpers(e echoInterface, assetHash string) map[string]interface{} {
 			return param.BuildPresetParamName(preset.Name)
 		},
 	}
+}
+
+func listDownloadRef(e echoInterface, token string) string {
+	return "https://get.letsblock.it" + href(e, "render-filterlist", token)
 }
 
 func href(e echoInterface, route string, args string) string {
