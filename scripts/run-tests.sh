f9927e34e98deebfbb7ee78517c0024519ea1b10
Xavier Vello <xavier.vello@gmail.com>
2022-05-24 21:10:59 +0200

    scripts: fail-fast CI on filter tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,6 +5,10 @@
 set -euox pipefail
 export GOGC=400
 
+# Fail-fast on filter changes
+go test ./src/filters
+
+# Full tests for backend changes
 golangci-lint run --timeout 5m
 go test -v -race ./...
 echo "OK"
