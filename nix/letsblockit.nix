764fd15cd69b96a6abf366b33d2c03b52e25993a
xvello <xvello@users.noreply.github.com>
2022-09-08 21:40:40 +0000

    upgrade external dependencies

--- nix/letsblockit.nix
+++ nix/letsblockit.nix
@@ -4,6 +4,6 @@ buildGoModule.override { go = go_1_18; } {
   pname = "letsblockit";
   src = ./..;
   subPackages = "cmd/" + cmd;
-  vendorSha256 = "sha256-NCpjoabxCf6gZ/RyePXPdnsMwjKsO2PonbXWX2jiqNk=";
+  vendorSha256 = "sha256-7o3zfphjqnmTqWSFdPWDFPoc7ezZxEKgj09GFD45XS0=";
   version = "1.0";
 }
