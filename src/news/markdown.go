3113b760b918a32c641760c254b8c7c29ed33437
Xavier Vello <xavier.vello@gmail.com>
2023-02-02 18:59:26 +0100

    news: detect template names in changelog and link to page (#340)

--- src/news/markdown.go
+++ src/news/markdown.go
@@ -9,22 +9,45 @@ import (
 )
 
 var (
-	githubLinkPrefix = []byte("https://github.com/")
-	githubRepoName   = "letsblockit"
+	githubLinkPrefix      = []byte("https://github.com/")
+	githubRepoName        = "letsblockit"
+	templateNameSeparator = []byte(":")
+	templateLinkPrefix    = []byte("https://letsblock.it/filters/")
 )
 
 type releaseNoteRenderer struct {
 	officialInstance bool
+	templateProvider templateProvider
 	*blackfriday.HTMLRenderer
 }
 
 func (r *releaseNoteRenderer) RenderNode(w io.Writer, node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
-	//nolint:exhaustive
-	switch node.Type {
+	switch node.Type { //nolint:exhaustive
 	case blackfriday.HorizontalRule:
 		if r.officialInstance {
 			// Stop parsing on the first hr, items below are not relevant on the official instance
 			return blackfriday.Terminate
+		}
+	case blackfriday.Item:
+		// Match for template names at the start of list items and make them a link
+		hasText := node.FirstChild != nil && node.FirstChild.Type == blackfriday.Paragraph &&
+			node.FirstChild.FirstChild != nil && node.FirstChild.FirstChild.Type == blackfriday.Text
+		if hasText {
+			textNode := node.FirstChild.FirstChild
+
+			maybeName, _, foundSeparator := bytes.Cut(textNode.Literal, templateNameSeparator)
+			if foundSeparator && r.templateProvider.Has(string(maybeName)) {
+				linkNode := blackfriday.NewNode(blackfriday.Link)
+				linkNode.LinkData.Destination = append(linkNode.LinkData.Destination, templateLinkPrefix...)
+				linkNode.LinkData.Destination = append(linkNode.LinkData.Destination, maybeName...)
+
+				linkTextNode := blackfriday.NewNode(blackfriday.Text)
+				linkTextNode.Literal = maybeName
+				linkNode.AppendChild(linkTextNode)
+
+				textNode.InsertBefore(linkNode)
+				textNode.Literal = textNode.Literal[len(maybeName):]
+			}
 		}
 	case blackfriday.Link:
 		// Match github pull/commit links and shorten the anchor text
@@ -62,10 +85,11 @@ func (r *releaseNoteRenderer) RenderNode(w io.Writer, node *blackfriday.Node, en
 	return r.HTMLRenderer.RenderNode(w, node, entering)
 }
 
-func initRenderer(officialInstance bool) blackfriday.Option {
+func initRenderer(officialInstance bool, tp templateProvider) blackfriday.Option {
 	return blackfriday.WithRenderer(
 		&releaseNoteRenderer{
 			officialInstance: officialInstance,
+			templateProvider: tp,
 			HTMLRenderer: blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
 				HeadingLevelOffset: 2,
 			}),
