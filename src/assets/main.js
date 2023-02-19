1b2f3ac4f738571535aee44591cc186c43a54c7e
Xavier Vello <xavier.vello@gmail.com>
2023-02-19 13:51:30 +0100

    slim down main.js by removing unused bootstrap imports

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,20 @@
 "use strict";
 
-import 'bootstrap'
 import 'htmx.org'
+
+// import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
+
 
 import './js/cond-visibility'
 import './js/hot-reload'
