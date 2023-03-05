15935ade69ff43290c08dca92c204ef9fdbca765
Xavier Vello <xavier.vello@gmail.com>
2023-03-05 13:57:23 +0100

    frontend: fix html-alert logic

--- src/assets/js/htmx-errors.js
+++ src/assets/js/htmx-errors.js
@@ -4,8 +4,8 @@
  * Hooks into HTMX afterRequest events and shows an alert div when requests fail.
  */
 
-const errorTarget = document.getElementById("htmx-alert")
 document.body.addEventListener('htmx:afterRequest', function (evt) {
+    const errorTarget = document.getElementById("htmx-alert")
     if (evt.detail.successful) {
         errorTarget.setAttribute("hidden", "true")
         errorTarget.innerText = "";
