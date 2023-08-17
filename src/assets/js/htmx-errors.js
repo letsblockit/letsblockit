e42b48f85bab7cc41aedf064758f2db5bceaf045
Xavier Vello <xavier.vello@gmail.com>
2023-08-17 13:04:55 +0200

    don't show error alert on htmx redirects

--- src/assets/js/htmx-errors.js
+++ src/assets/js/htmx-errors.js
@@ -6,15 +6,16 @@
 
 document.body.addEventListener('htmx:afterRequest', function (evt) {
     const errorTarget = document.getElementById("htmx-alert")
-    if (evt.detail.successful) {
-        errorTarget.setAttribute("hidden", "true")
-        errorTarget.innerText = "";
-    } else {
+    if (evt.detail.error || evt.detail.failed) {
         if (evt.detail.xhr.statusText) {
             errorTarget.innerText = "Unexpected server error: " + evt.detail.xhr.statusText;
         } else {
             errorTarget.innerText = "Unexpected network error, check your connection and try again.";
         }
         errorTarget.removeAttribute("hidden");
+    } else {
+        const errorTarget = document.getElementById("htmx-alert")
+        errorTarget.setAttribute("hidden", "true")
+        errorTarget.innerText = "";
     }
 });
