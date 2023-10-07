ca4935b61c40152d0d1aa7a572ba73b2a2d9732b
Xavier Vello <xavier.vello@gmail.com>
2023-10-07 13:42:13 +0200

    server: improve htmx error alerts

--- src/assets/js/htmx-errors.js
+++ src/assets/js/htmx-errors.js
@@ -6,16 +6,20 @@
 
 document.body.addEventListener('htmx:afterRequest', function (evt) {
     const errorTarget = document.getElementById("htmx-alert")
-    if (evt.detail.error || evt.detail.failed) {
-        if (evt.detail.xhr.statusText) {
-            errorTarget.innerText = "Unexpected server error: " + evt.detail.xhr.statusText;
-        } else {
-            errorTarget.innerText = "Unexpected network error, check your connection and try again.";
-        }
-        errorTarget.removeAttribute("hidden");
-    } else {
-        const errorTarget = document.getElementById("htmx-alert")
+    if (evt.detail.successful) {
+        // Successful request, clear out alert
         errorTarget.setAttribute("hidden", "true")
         errorTarget.innerText = "";
+    } else if (evt.detail.failed && evt.detail.xhr) {
+        // Server error with response contents, equivalent to htmx:responseError
+        console.warn("Server error", evt.detail)
+        const xhr = evt.detail.xhr;
+        errorTarget.innerText = `Unexpected server error: ${xhr.status} - ${xhr.statusText}`;
+        errorTarget.removeAttribute("hidden");
+    } else {
+        // Unspecified failure, usually caused by network error
+        console.error("Unexpected htmx error", evt.detail)
+        errorTarget.innerText = "Unexpected error, check your connection and try to refresh the page.";
+        errorTarget.removeAttribute("hidden");
     }
 });
